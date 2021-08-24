f4ea47f57b793c61f9d9a3a4b4d10b2b10cd6a9c
Marharyta kulinich <margaret.kulinich@gmail.com>
2021-08-24 12:56:54 +0300

    complete 3rd module 3rd task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+  let strArr = str.split('-');
+
+  let strRestCapitalized = strArr.slice(1)
+    .map(strItem => strItem[0].toUpperCase() + strItem.slice(1))
+    .join('');
+
+  return strArr[0] + strRestCapitalized;
 }
+
+

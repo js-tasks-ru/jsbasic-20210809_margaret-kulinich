c2b978a89f201ade21302023ab19b584d58b16a4
Marharyta kulinich <margaret.kulinich@gmail.com>
2021-09-03 17:36:21 +0300

    complete 5th module 3rd task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,40 @@
 function initCarousel() {
-  // ваш код...
+  const carouselArrowButtonRight = document.querySelector('.carousel__arrow_right');
+  const carouselArrowButtonLeft = document.querySelector('.carousel__arrow_left');
+  const carouselInner = document.querySelector('.carousel__inner');
+  const carouselSlides = document.querySelectorAll('.carousel__slide');
+  const carouselSlideFirst = 0;
+  const carouselSlideLast = carouselSlides.length - 1;
+  let carouselSlideCurrent = carouselSlideFirst;
+  let carouselInnerTranslate = 0;
+
+  carouselArrowButtonLeft.style.display = 'none';
+
+  carouselInner.style.transform = `translateX(${carouselInnerTranslate}px)`;
+
+  carouselArrowButtonRight.addEventListener('click', () => {
+    carouselInnerTranslate -= carouselInner.offsetWidth;
+    carouselInner.style.transform = `translateX(${carouselInnerTranslate}px)`;
+    carouselSlideCurrent++;
+    
+    if (carouselSlideCurrent === carouselSlideLast) {
+      carouselArrowButtonRight.style.display = 'none';
+    } else {
+      carouselArrowButtonLeft.style.display = '';
+    }
+  });
+
+  carouselArrowButtonLeft.addEventListener('click', () => {
+    carouselInnerTranslate += carouselInner.offsetWidth;
+    carouselInner.style.transform = `translateX(${carouselInnerTranslate}px)`;
+    carouselSlideCurrent--;
+    carouselArrowButtonRight.style.display = '';
+    
+    if (carouselSlideCurrent === carouselSlideFirst) {
+      carouselArrowButtonLeft.style.display = 'none';
+    } else {
+      carouselArrowButtonLeft.style.display = '';
+    }
+  });
+
 }
